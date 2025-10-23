1ef38ca8c9965fa362e34156bf2ceb3092833ca1
DarioConiglione <dariocng13@gmail.com>
2025-10-23 16:03:34 +0100

    Create with loop self generated emails from api

---
+++ asset/js/script.js
@@ -0,0 +1,16 @@
+ const emailsEL = document.getElementById("emails");
+
+    for (let i = 0; i < 10; i++) {
+      axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+
+        .then(response => {
+          const email = response.data.response;
+          const li = document.createElement("li");
+          li.textContent = email;
+          emailsEL.appendChild(li);
+        })
+
+        .catch(error => {
+          console.error("Errore:", error);
+        });
+    }
