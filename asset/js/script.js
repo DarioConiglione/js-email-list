4eb55808816b9e9e983761d00ab6cc8f0f8fa044
DarioConiglione <dariocng13@gmail.com>
2025-10-24 09:39:30 +0100

    add button for create again emails

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,5 +1,9 @@
  const emailsEL = document.getElementById("emails");
+ 
 
+ function randomEmails() {
+ emailsEL.innerHTML = "";
+ 
     for (let i = 0; i < 10; i++) {
       axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
 
@@ -8,9 +12,16 @@
           const li = document.createElement("li");
           li.textContent = email;
           emailsEL.appendChild(li);
-        })
+          
+        })      
 
         .catch(error => {
           console.error("Errore:", error);
-        });
+        })
     }
+}
+
+const buttonEl = document.getElementById("button");
+
+buttonEl.addEventListener("click", randomEmails)
+
